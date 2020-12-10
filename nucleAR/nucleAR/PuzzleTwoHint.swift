6d932c03e32234123a011d05fb64b60d6ea3744e
Shifath Salam <shifathsalam@gmail.com>
2020-12-10 14:11:39 -0800

    added segues and view controllers

---
+++ nucleAR/nucleAR/PuzzleTwoHint.swift
@@ -0,0 +1,16 @@
+//
+//  PuzzleTwoHint.swift
+//  nucleAR
+//
+//  Created by Shifath Salam on 12/9/20.
+//
+
+import Foundation
+import UIKit
+
+class PuzzleTwoHint: UIViewController{
+    @IBOutlet weak var clue: UILabel!
+    override func viewDidLoad() {
+    self.view.addSubview(clue)
+    }
+}
--- nucleAR/nucleAR/ViewController.swift
+++ nucleAR/nucleAR/ViewController.swift
@@ -10,6 +10,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var arMagicView: ARView!
     @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var navButton: UIButton!
     
     //setup variables for timer
     var timer: Timer?
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navButton.frame.origin = CGPoint(x:100,y:100)
+        self.view.addSubview(navButton)
         
         // configuring timer label
         timerLabel.frame = CGRect(x:10, y:0, width: 500, height: 200)
@@ -27,78 +30,12 @@ class ViewController: UIViewController {
         timerLabel.center = CGPoint(x: 300, y: 50)
         beginTimer()
         
-        // configuring tap gesture recognizer
-        let itemTap = UITapGestureRecognizer(target: self, action: #selector(Tappy))
-        view.addGestureRecognizer(itemTap)
-        
         //setting up anchor for Puzzle 1
         anchorPuzzle1 = try! Puzzle1.loadP1()
         anchorPuzzle1.generateCollisionShapes(recursive: true)
         arMagicView.scene.anchors.append(anchorPuzzle1)
         
-        //setting up anchor with an AnchorEntity
-        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
-           arMagicView.scene.addAnchor(anchor)
-        
-        //setting up elements
-        var elements: [Entity] = []
-        for _ in 1...4 {
-               let box = MeshResource.generateBox(width: 0.04, height: 0.002, depth: 0.04)
-               let blackMaterial = SimpleMaterial(color: .black, isMetallic: true)
-               let model = ModelEntity(mesh: box, materials: [blackMaterial])
-               model.generateCollisionShapes(recursive: true)
-               elements.append(model)
-        }
-        
-        //setting up positioning for each element
-        for (idx, element) in elements.enumerated() {
-               let x = Float(idx % 4) - 1.5
-               let z = Float(idx / 4) - 1.5
-               element.position = [x*0.1, 0, z*0.1]
-               anchor.addChild(element)
-        }
-           
-        //configuring square platform for each element
-        let squareSize: Float = 0.7
-        let squareMesh = MeshResource.generateBox(size: squareSize)
-        let occlusionSquare = ModelEntity(mesh: squareMesh, materials: [OcclusionMaterial()])
-           occlusionSquare.position.y = -squareSize/2
-        anchor.addChild(occlusionSquare)
-           
-           
-        // creating cancellable to stop side effects
-        var cancellable: AnyCancellable? = nil
-           //loading usdz models
-           cancellable = ModelEntity.loadModelAsync(named: "01")
-            .append(ModelEntity.loadModelAsync(named: "02"))
-            .append(ModelEntity.loadModelAsync(named: "03"))
-            .append(ModelEntity.loadModelAsync(named: "04"))
-           .collect()
-               .sink(receiveCompletion: {error in
-                   print("Error: \(error)")
-                   cancellable?.cancel()
-               }, receiveValue: { items in
-                   var objects: [ModelEntity] = []
-                   for item in items {
-                       item.setScale(SIMD3<Float>(0.002, 0.002, 0.002), relativeTo: anchor)
-                       item.generateCollisionShapes(recursive: true)
-                       objects.append(item.clone(recursive: true))
-                   }
-                   //setting a name for each object
-                   for (idx,obj) in objects.enumerated() {
-                       obj.name = "entity_\(idx)"
-                   }
-                   //randomizing objects
-                   objects.shuffle()
-                   for (idx, obj) in objects.enumerated() {
-                       elements[idx].addChild(obj)
-                    if(obj.name=="entity_1"){
-                        elements[idx].name = "entity_1" 
-                   }
-                       print(elements[idx])
-                   }
-                   cancellable?.cancel()
-               })
+        anchorPuzzle1.notifications.puzzle1Notification.post()
        }
     
     //function to execute timer
@@ -130,43 +67,8 @@ class ViewController: UIViewController {
          return String(format: "%02d:%02d", min, sec)
      }
     
-    // Tap function for puzzle 2 items
-    @IBAction func Tappy(_ sender: UITapGestureRecognizer) {
-        
-        //start Puzzle 1
-        anchorPuzzle1.notifications.puzzle1Notification.post()
-
-        // locate where the user is tapping
-        let tapLocation = sender.location(in: arMagicView)
-                // configuring success label for the right answer
-                let success = UILabel(frame: CGRect(x: 0, y: 0, width: 500, height: 22))
-                success.textColor = UIColor.white
-                success.backgroundColor = UIColor.systemGreen
-                success.center = CGPoint(x: 200, y: 10)
-                success.textAlignment = .center
-                // configuring error label for the wrong answer
-                let error = UILabel(frame: CGRect(x: 0, y: 0, width: 500, height: 22))
-                error.textColor = UIColor.white
-                error.backgroundColor = UIColor.red
-                error.center = CGPoint(x: 200, y: 10)
-                error.textAlignment = .center
-                if let element = arMagicView.entity(at: tapLocation) {
-                    // add time on correct choice or remove time on wrong choice
-                    if element.name == "entity_1" {
-                        totalTime += 10
-                        success.text = "Correct! +10s"
-                        self.view.addSubview(success)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                            success.removeFromSuperview()
-                        }
-                    }else{
-                        totalTime -= 10
-                        error.text = "Incorrect! -10s"
-                        self.view.addSubview(error)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                            error.removeFromSuperview()
-                        }
-                    }
-            }
-    } // Tappy function
+   
+    @IBAction func TapDeallocate(_ sender: UIButton) {
+        self.dismiss(animated:true, completion: nil)
+    }
 }
