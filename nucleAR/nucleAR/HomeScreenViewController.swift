b805afe97333e6cf9b1b29e3c32fd1b3a9bb93b7
David Eisenbaum <davidjeisenbaum@gmail.com>
2020-12-13 04:00:39 -0800

    visit website button working

--- nucleAR/nucleAR/HomeScreenViewController.swift
+++ nucleAR/nucleAR/HomeScreenViewController.swift
@@ -8,6 +8,7 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
 
+    @IBOutlet weak var websiteButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var logo: UIImageView!
     override func viewDidLoad() {
@@ -19,9 +20,22 @@ class HomeScreenViewController: UIViewController {
         startButton.layer.cornerRadius = 18.0
         startButton.layer.borderWidth = 1.0
         startButton.clipsToBounds = true
+        
+        // Customize button
+        websiteButton.frame = CGRect(x: 160, y: 160, width: 160, height: 160)
+        websiteButton.layer.cornerRadius = 18.0
+        websiteButton.layer.borderWidth = 1.0
+        websiteButton.clipsToBounds = true
     }
     
-
+    @IBAction func visitWebsiteButton(_ sender: Any) {
+        if let url = URL(string: "https://navigatingnuclear.com/nuclear-reimagined-vft/") {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:])
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
