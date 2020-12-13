333f74e06fd84a4e342d7249fa04f2b3514741c4
Johanna <johannaguevara17@gmail.com>
2020-12-13 07:16:39 -0800

    Progress with the notifications

--- nucleAR/nucleAR/ViewController.swift
+++ nucleAR/nucleAR/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
     var timer: Timer?
     var totalTime = 60
     
-    //setup variables for Puzzle 1
+//    func triggerAction(){
+//        if let anchorPuzzle1 = arMagicView.scene.anchors[0] as? MyScene.Box2{
+//            anchorPuzzle1.notifications.triggerAction.post()
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,12 +30,30 @@ class ViewController: UIViewController {
         timerLabel.center = CGPoint(x: 300, y: 50)
         beginTimer()
         
+//        let anchorPuzzle1 = try! MyScene.loadBox2()
+//        anchorPuzzle1.actions.reactorEnding.onAction
+        
         var anchorPuzzle1: MyScene.Box!
         anchorPuzzle1 = try! MyScene.loadBox()
         anchorPuzzle1.generateCollisionShapes(recursive: true)
         arMagicView.scene.addAnchor(anchorPuzzle1)
+        //arMagicView.scene.anchors.append(anchorPuzzle1)
+        
+        
+        
+        //anchorPuzzle1.actions.ReactorEnding.onAction = handleTapOnEntity(_:)
+        
+//        if(anchorPuzzle1.scene?.name == "ARView.scene-4"){
+//            print("okay")
+//        }
+//        //4389697504 aka ARView.scene-4 aka Box2
     }
-       
+    
+//    func handleTapOnEntity(_ entity: Entity?) {
+//         guard let entity = entity else { return }
+//         print("Made it")
+//     }
+    
     //function to execute timer
     func beginTimer() {
              self.totalTime = 120
